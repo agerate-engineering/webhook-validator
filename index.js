3eb2167a60b12501fe82066df0ca5d0a4e73c640
peterk9 <kevpeters1@gmail.com>
2019-07-04 18:46:36 -0400

    minor tweaks

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const crypto = require('crypto');
 const safeCompare = require('safe-compare');
 
-const ValidateWebhook = async (request, secret, algorithm = 'sha256', encoding = 'utf8', hmacHeader = 'X-Shopify-Hmac-Sha256') => {
+const ValidateWebhook = async (request, secret, options) => {
     console.debug(`validating incoming webhook.`);
+
+    const { algorithm, encoding, hmacHeader } = options;
     const hmacHeader = request.get(hmacHeader);
     const body = request.rawBody;
     const generatedHash = crypto
         .createHmac(algorithm, secret)
         .update(body, encoding)
         .digest('base64');
+
     if (!safeCompare(generatedHash, hmacHeader)) {
         const ip = request.ip;
         const userAgent = request.get('User-Agent');
